f186662cb6ebf225fb0d3b2af6f1f2d5570b0e11
Sydney <sydneyparsonsyb@gmail.com>
2021-07-28 16:55:44 -0400

    user_conroller_spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe UsersController, type: :controller do
   describe 'GET #new' do
     it 'renders the new links page' do
-      get :new, link: {}
+      get :new
 
       expect(response).to render_template('new')
       expect(response).to have_http_status(200)
@@ -13,10 +13,27 @@ RSpec.describe UsersController, type: :controller do
   describe 'POST #create' do
     context 'with invalid params' do
       it 'validates the presence of user and body' do
-        post :create, params: { link: { title: 'this is an invalid link'}}
+        post :create, params: { user: { usename: 'grey', password: ''}}
         expect(response).to render_template('new')
         expect(flash[:errors]).to be_present
       end
     end
+    context 'with valid params' do
+      it 'redirects the user to users index on success' do
+        post :create, params: { user: { usename: 'greys', password: 'password'}}
+        expect(response).to redirect_to(users_url)
+      end
+
+      it 'logs in the user' do
+        post :create, params: { user: { usename: 'greys', password: 'password'}}
+        user = User.find_by_credentials('greys', 'password')
+        expect(session[:session_token]).to eq(user.session_token)
+      end
   end
+
+  subject (:joe) { User.create!(username: 'joe', password: 'password') }
+
+  let (:sally) { User.create!(username: 'sally', password: 'password') }
+   
+  describe 'POST'
 end
